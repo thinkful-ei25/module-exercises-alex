a5c8a8db350f90f4f0aa45c78f73bd88161cbfb3
talexcrowell <talexcrowell@gmail.com>
2018-10-01 08:58:11 -0700

    finished second namespace exercise

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* global store, cuid */
+/* global store, cuid, Item*/
 
 // eslint-disable-next-line no-unused-vars
 const shoppingList = (function(){
@@ -58,7 +58,12 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try {
+      Item.validateName(itemName);
+      store.items.push(Item.create(itemName));
+    } catch(error) {
+      console.log('Cannot add item: '+ error.message);
+    }
   }
   
   function handleNewItemSubmit() {
